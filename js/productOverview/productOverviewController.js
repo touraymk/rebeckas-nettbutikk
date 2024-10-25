6d0a794196e38b89ae4f9f04c104ce5ae6109b6d
Kristian Bjerkelund Olsen <kristianbjolsen@gmail.com>
2024-10-25 09:58:45 +0200

    Update productOverviewController.js

---
+++ js/productOverview/productOverviewController.js
@@ -0,0 +1,26 @@
+<div id="productPage">
+<div id="productName">Produkt</div>
+
+<div id="productAlbum">
+    <img src="" alt=""></img>
+    <img src="" alt=""></img>
+    <img src="" alt=""></img>
+</div>
+
+<div id="productSize">
+    <button id="btnSmall">Small</button>
+    <button id="btnMedium">Medium</button>
+    <button id="btnLarge">Large</button>
+</div>
+
+<div id="orderVolum">
+    <button onclick="minus()">-</button>
+    <button onclick="plus()">+</button>
+</div>
+
+<div id="productPrice">0,-</div>
+
+<div id="productInfo"></div>
+
+<div id="deliveryTime">1-2 dager</div>
+</div>
